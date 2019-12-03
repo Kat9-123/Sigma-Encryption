7ce1661a78fed9216586456202dcc4537c891dbf
Kat9_123 <56130489+Kat9-123@users.noreply.github.com>
2019-12-03 10:13:58 +0100

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -13,10 +13,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+
 public class Main : MonoBehaviour
 {
+    #region Public variables
     public string Input; //Input to encrypt/decrypt
     public string Output; //The output
+    [Space]
+    public bool Start;
     [Space]
     public bool createkey;
     public bool encrypt;
@@ -24,39 +28,56 @@ public class Main : MonoBehaviour
     [Space]
     public string KeyPath; //The path to the key/where to make it
     public int SegmentAmount; //Amount of 'Segments' (layers)
-    private string[] Key; //An array for all lines in the key.sigma file
-    private List<string> Segments = new List<string>();
+    #endregion
+    #region Private variables
     private readonly string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private List<char> SwitchAplha = new List<char>();
-    private List<char> Input_List = new List<char>(); //String list of all letters inputted (empty) 
-
-    private void Start()
+    #endregion
+    #region StartUp
+    private void Awake()
     {
-        StartUp();
-        if(createkey) CreateKey(); 
-        if(encrypt) Encrypt(); 
-        if(decrypt) Decrypt();
+        Debug.LogWarning("Ready");
     }
-    void StartUp()
+    private void Update()
     {
-        //Makes the input IN CAPS
+        if (Start) _Start();
+    }
+    void _Start()
+    {
+        Debug.LogWarning("Starting...");
         Input = Input.ToUpper();
- 
-        //Slits the Input(String) into Input_List(Char)
-        for (int i = 0; i < Input.Length; i++) 
+        Output = null;
+        System.Diagnostics.Stopwatch st = new System.Diagnostics.Stopwatch();
+        st.Start();
+        if (createkey)
         {
-            Input_List.Add(Input[i]); 
+            CreateKey();
+            createkey = false;
         }
+        if (encrypt)
+        {
+            Encrypt();
+            encrypt = false;
+        }
+        if (decrypt)
+        {
+            Decrypt();
+            decrypt = false;
+        }
+        st.Stop();
+        Debug.LogWarning("Done in: " + st.ElapsedMilliseconds + " Miliseconds");
+        Start = false;  
     }
+    #endregion
+
 
     #region Key Making
     void CreateKey()
     {
-        CreateDecider();
         string Decider = CreateDecider();
-        CreateSalt();
-        CreateSegments();
-        WriteKey(Decider);
+        string FirstSalt = Random.Range(10, 64) + "\n";
+        string LastSalt = Random.Range(10, 64) + "\n";
+        List<string> Segments = CreateSegments();
+        WriteKey(Decider, FirstSalt, LastSalt, Segments);   
     }
     string CreateDecider()
     {
@@ -64,25 +85,11 @@ public class Main : MonoBehaviour
         string DeciderStr = DeciderInt + "\n"; //String to hold the decider
         return DeciderStr;
     }
-    void CreateSalt()
-    {
-        /*
-//Creating Salt
-int saltAmount = Random.Range(6, 32);
-List<int> SaltLocation = new List<int>();
-List<int> SaltLenght = new List<int>();
-
-for (int l = 0; l < saltAmount; l++)
-{
-    SaltLocation.Add(Random.Range(0, 100));
-    SaltLenght.Add(Random.Range(0, 10));
-}
-*/
-    }
-    void CreateSegments()
+    List<string> CreateSegments()
     {
         string[] SegmentA = new string[26]; //This segment letters in an array
         string Segment = ""; //Segment string
+        List<string> Segments = new List<string>();
         for (int i = 0; i < SegmentAmount; i++)
         {
             for (int z = 0; z < 26; z++)
@@ -105,13 +112,16 @@ for (int l = 0; l < saltAmount; l++)
             Segments.Add(Segment);
             Segment = null;
         }
+        return Segments;
     }
-    void WriteKey(string Decider)
+    void WriteKey(string Decider, string FirstSalt, string LastSalt, List<string> Segments)
     {
         string path = KeyPath + "/key.sigma"; //Real path (path + /key.sigma)
         if (!File.Exists(path)) //If the file doesn't already exist
         {
-            File.WriteAllText(path, "" + Decider); //Writes the decider string to the file
+            File.WriteAllText(path, Decider); //Writes the decider string to the file
+            File.AppendAllText(path, FirstSalt);
+            File.AppendAllText(path, LastSalt);
             for (int x = 0; x < SegmentAmount; x++) //For each segment
             {
                 File.AppendAllText(path, Segments[x]); //Adds the segment to the file
@@ -119,15 +129,17 @@ for (int l = 0; l < saltAmount; l++)
         }
     }
     #endregion
-
-    void ReadKey()
+    #region All
+    string[] ReadKey()
     {
-        Key = File.ReadAllText(KeyPath + "/key.sigma").Split("\n"[0]);
+        return File.ReadAllText(KeyPath + "/key.sigma").Split("\n"[0]);
     }
-    void DeciderSwitch(int z)
+    List<char> DeciderSwitch(int z, string[] Key)
     {
         int decider = int.Parse(Key[0]); //Parses the decider to an int
-        SwitchAplha.Clear(); //Clears the SwitchAlpha list
+        List<char> SwitchAlpha = new List<char>();
+        Debug.Log("Switched by: " + decider * z);
+        SwitchAlpha.Clear(); //Clears the SwitchAlpha list
         int shiftAmount = z * decider; //Amount of input * the decider
         if (shiftAmount > 26)
         {
@@ -139,66 +151,100 @@ for (int l = 0; l < saltAmount; l++)
         //Adds the last shiftAmount letters of the Alphabet to SwitchAlpha
         for (int j = 26 - shiftAmount; j < 26; j++)
         {
-            SwitchAplha.Add(Alphabet[j]); //Add the letter from AlphaList (main alphabet)
+            SwitchAlpha.Add(Alphabet[j]); //Add the letter from AlphaList (main alphabet)
         }
         //Adds the rest of the letters
         for (int u = 0; u < 26 - shiftAmount; u++)
         {
-            SwitchAplha.Add(Alphabet[u]);
+            SwitchAlpha.Add(Alphabet[u]);
         }
+        return SwitchAlpha;
     }
-
-    void Decrypt()
-    {
-        ReadKey();
-        for (int z = 0; z < Input_List.Count; z++) //For each letter in the input list
-        {
-            DeciderSwitch(z);
-            char CurrentLetterProcess = Input_List[z];
-            for (int h = Key.Length - 2; h > 0; h--)
-            {
-                for (int g = 0; g < 26; g++) //For each letter in said segment
-                {
-                    if (Key[h][g] == CurrentLetterProcess) //If the that is being processed currently is equal to a letter in the alphabet. g=the index of said letter
-                    {
-                        if(h == 1) CurrentLetterProcess  = SwitchAplha[g];
-                        else CurrentLetterProcess = Alphabet[g];
-                    }
-                }
-            }
-            Output += CurrentLetterProcess;
-        }
-    }
-
+    #endregion
+    #region Encryption
     void Encrypt()
     {
-        ReadKey();
-        for (int z = 0; z < Input_List.Count; z++) //For each letter in the input list
+        string[] Key = ReadKey();
+        for (int z = 0; z < Input.Length; z++) //For each letter in the input list
         {
-            DeciderSwitch(z);
-            char CurrentLetterProcess = FirstInputEncryption(z);
-            for (int h = 2; h <= Key.Length - 2; h++)
+            char CurrentLetterProcess = FirstInputEncryption(z, Key);
+            char TempLetterProcess = 'a';
+            for (int h = 4; h <= Key.Length - 4; h++)
             {
                 for (int g = 0; g < 26; g++) //For each letter in said segment
                 {
                     if (CurrentLetterProcess == Alphabet[g]) //If the that is being processed currently is equal to a letter in the alphabet. g=the index of said letter
                     {
-                        CurrentLetterProcess =  Key[h][g];  //secondEnc is the g index in said string
+                        TempLetterProcess = Key[h][g];  //secondEnc is the g index in said string
                     }
                 }
+                CurrentLetterProcess = TempLetterProcess;
             }
             Output += CurrentLetterProcess;
         }
+        AddSalt(Key);
     }
-    char FirstInputEncryption(int z)
+    char FirstInputEncryption(int z, string [] Key)
     {
+        List<char> SwitchAlpha = DeciderSwitch(z, Key);
         for (int g = 0; g < 26; g++) //For each letter in said segment
         {
-            if (Input_List[z] == SwitchAplha[g]) //If the that is being processed currently is equal to a letter in the alphabet. g=the index of said letter
+            if (Input[z] == SwitchAlpha[g]) //If the that is being processed currently is equal to a letter in the alphabet. g=the index of said letter
             {
-                return Key[1][g];  //secondEnc is the g index in said string
+                return Key[3][g];  //secondEnc is the g index in said string
             }
         }
         return 'a';
     }
+    void AddSalt(string[] Key)
+    {
+        string TempOutput = Output;
+        Output = null;
+        for (int i = 0; i < int.Parse(Key[1]); i++)
+        {
+            Output += Alphabet[Random.Range(0, 26)];
+        }
+        Output += TempOutput;
+        for (int i = 0; i < int.Parse(Key[2]); i++)
+        {
+            Output += Alphabet[Random.Range(0, 26)];
+        }
+    }
+    #endregion
+    #region Decryption
+    void Decrypt()
+    {
+        string[] Key = ReadKey();
+        string InternalInput = RemoveSalt(Key);
+        for (int z = 0; z < InternalInput.Length; z++) //For each letter in the input list
+        {
+            List<char> SwitchAlpha = DeciderSwitch(z, Key);
+            char CurrentLetterProcess = InternalInput[z];
+            char TempLetterProcess = 'a';   
+            for (int h = Key.Length - 4; h > 2; h--)
+            {
+                for (int g = 0; g < 26; g++) //For each letter in said segment
+                {
+                    if (Key[h][g] == CurrentLetterProcess) //If the that is being processed currently is equal to a letter in the alphabet. g=the index of said letter
+                    {
+                        if (h == 3) TempLetterProcess = SwitchAlpha[g];
+                        else TempLetterProcess = Alphabet[g];
+                    }
+                }
+                CurrentLetterProcess = TempLetterProcess;
+            }
+            Output += CurrentLetterProcess;
+        }
+    }
+    string RemoveSalt(string[] Key)
+    {
+        string TempInput = "";
+        int Max = int.Parse(Key[1]) + (Input.Length - (int.Parse(Key[1]) + int.Parse(Key[2])));
+        for (int i = int.Parse(Key[1]); i < Max; i++)
+        {
+            TempInput += Input[i];
+        }
+        return TempInput;
+    }
+    #endregion
 }
